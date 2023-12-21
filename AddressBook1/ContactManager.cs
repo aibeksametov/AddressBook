24ae24e3c269d55c8d39dbd0a39cf5e250eade82
samet <samet@DESKTOP-OEAKA56>
2023-12-21 16:05:37 +0600

    Add project files.

---
+++ AddressBook1/ContactManager.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace AddressBook
+{
+    // Класс ContactManager отвечает за управление контактами
+    public class ContactManager
+    {
+        // Список контактов
+        private List<Contact> contacts = new List<Contact>();
+
+        // Метод для добавления нового контакта
+        public void AddContact(Contact contact)
+        {
+            // Проверка наличия контакта с таким же номером телефона
+            if (contacts.Any(c => c.PhoneNumber == contact.PhoneNumber))
+            {
+                Console.WriteLine("A contact with such a phone number already exists.");
+            }
+            else
+            {
+                // Добавление контакта в список и вывод сообщения об успешном добавлении
+                contacts.Add(contact);
+                Console.WriteLine("The contact has been added successfully.");
+            }
+        }
+
+        // Метод для удаления контакта по идентификатору (имени, фамилии или номеру телефона)
+        public void DeleteContact(string identifier)
+        {
+            Contact contact;
+
+            // Попытка преобразовать идентификатор в число (проверка на номер телефона)
+            if (int.TryParse(identifier, out _))
+            {
+                contact = contacts.FirstOrDefault(c => c.PhoneNumber == identifier);
+            }
+            else
+            {
+                // Поиск контакта по имени или фамилии
+                contact = contacts.FirstOrDefault(c => c.FirstName == identifier || c.LastName == identifier);
+            }
+
+            // Если контакт найден, он удаляется из списка
+            if (contact != null)
+            {
+                contacts.Remove(contact);
+                Console.WriteLine("The contact was successfully deleted.");
+            }
+            else
+            {
+                Console.WriteLine("The contact was not found.");
+            }
+        }
+
+        // Метод для редактирования контакта по номеру телефона
+        public void EditContact(string phoneNumber, Contact newContactData)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.PhoneNumber == phoneNumber);
+
+            // Если контакт найден, происходит редактирование
+            if (contact != null)
+            {
+                contact.FirstName = newContactData.FirstName;
+                contact.LastName = newContactData.LastName;
+                contact.PhoneNumber = newContactData.PhoneNumber;
+                contact.Email = newContactData.Email;
+                contact.Address = newContactData.Address;
+
+                Console.WriteLine("The contact has been successfully edited.");
+            }
+            else
+            {
+                Console.WriteLine("The contact was not found.");
+            }
+        }
+
+        // Метод для отображения всех контактов
+        public void DisplayAllContacts()
+        {
+            // Проверка наличия контактов для отображения
+            if (contacts.Count > 0)
+            {
+                Console.WriteLine("Contact List:");
+                // Вывод контактов в алфавитном порядке
+                foreach (var contact in contacts.OrderBy(c => c.FirstName))
+                {
+                    Console.WriteLine($"FirstName: {contact.FirstName}, LastName: {contact.LastName},Phone Number: {contact.PhoneNumber}, Email: {contact.Email}, Address: {contact.Address}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no contacts to display.");
+            }
+        }
+
+        // Метод для поиска контакта по идентификатору (имени, фамилии или номеру телефона)
+        public Contact FindContact(string identifier)
+        {
+            return contacts.FirstOrDefault(c => c.FirstName == identifier || c.LastName == identifier || c.PhoneNumber == identifier);
+        }
+
+        // Метод для сохранения контактов в файл
+        public void SaveContactsToFile(string filePath)
+        {
+            // Сериализация списка контактов в формат JSON и сохранение в файл
+            string json = JsonConvert.SerializeObject(contacts, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(filePath, json);
+            Console.WriteLine("Contacts have been successfully saved to a file.");
+        }
+
+        // Метод для загрузки контактов из файла
+        public void LoadContactsFromFile(string filePath)
+        {
+            // Проверка наличия файла перед загрузкой
+            if (File.Exists(filePath))
+            {
+                // Чтение JSON из файла, десериализация в список контактов
+                string json = File.ReadAllText(filePath);
+                contacts = JsonConvert.DeserializeObject<List<Contact>>(json);
+                Console.WriteLine("Contacts have been successfully uploaded from the file.");
+            }
+            else
+            {
+                Console.WriteLine("The file was not found. The download is not possible.");
+            }
+        }
+
+        // Метод для отправки электронного письма контакту
+        public void SendEmail(Contact contact, string subject, string body)
+        {
+            // Проверка наличия адреса электронной почты у контакта
+            if (!string.IsNullOrEmpty(contact.Email))
+            {
+                try
+                {
+                    // Настройка и отправка электронного письма через SMTP
+                    using (var client = new SmtpClient("smtp.example.com"))
+                    {
+                        client.Port = 587;
+                        client.Credentials = new NetworkCredential("your_email@example.com", "your_password");
+                        client.EnableSsl = true;
+
+                        var message = new MailMessage("your_email@example.com", contact.Email, subject, body);
+                        client.Send(message);
+
+                        Console.WriteLine($"The email was successfully sent to {contact.Email}.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error when sending an email: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The contact does not have an email address.");
+            }
+        }
+    }
+}
---
+++ AddressBook1/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net;//Пространство имен System.Net было добавлено, чтобы использовать классы MailMessage и SmtpClient для отправки электронных писем
+using System.Net.Mail;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ContactManager contactManager = new ContactManager();
+
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Add contact");
+                Console.WriteLine("2. Delete contact");
+                Console.WriteLine("3. Edit contact");
+                Console.WriteLine("4. Search");
+                Console.WriteLine("5. Get all contacts");
+                Console.WriteLine("6. Save to file");
+                Console.WriteLine("7. Load contacts");
+                Console.WriteLine("8. Sent Email");
+                Console.WriteLine("9. Exit");
+                //Каждая операция выполняется в бесконечном цикле (while (true)) с выходом из программы при выборе пользователя "9".
+                Console.Write("Select an action (1-9): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddContact(contactManager);
+                        break;
+                    case "2":
+                        DeleteContact(contactManager);
+                        break;
+                    case "3":
+                        EditContact(contactManager);
+                        break;
+                    case "4":
+                        SearchContact(contactManager);
+                        break;
+                    case "5":
+                        contactManager.DisplayAllContacts();
+                        break;
+                    case "6":
+                        SaveContactsToFile(contactManager);
+                        break;
+                    case "7":
+                        LoadContactsFromFile(contactManager);
+                        break;
+                    case "8":
+                        SendEmail(contactManager);
+                        break;
+                    case "9":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice. Please select from 1 to 9.");
+                        break;
+                }
+                Console.ReadLine();
+            }
+
+        }
+
+        static void AddContact(ContactManager contactManager)
+            //AddContact: Запрашивает у пользователя информацию о новом контакте и добавляет его в менеджер контактов.
+        {
+            Console.Write("FirstName: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("LastName: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+
+            Contact newContact = new Contact
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                PhoneNumber = phoneNumber,
+                Email = email,
+                Address = address
+            };
+
+            contactManager.AddContact(newContact);
+        }
+
+        static void DeleteContact(ContactManager contactManager)
+            //DeleteContact: Запрашивает у пользователя идентификатор контакта (имя, фамилию или номер телефона) и удаляет соответствующий контакт.
+        {
+            Console.Write("Enter the first name, last name, or phone number of the contact to delete: ");
+            string identifier = Console.ReadLine();
+
+            contactManager.DeleteContact(identifier);
+        }
+
+        static void EditContact(ContactManager contactManager)
+            //EditContact: Запрашивает у пользователя номер телефона для редактирования существующего контакта, а затем предоставляет возможность ввести новые данные для контакта.
+        {
+            Console.Write("Enter the phone number of the contact to edit: ");
+            string phoneNumber = Console.ReadLine();
+
+            Contact existingContact = contactManager.FindContact(phoneNumber);
+
+            if (existingContact != null)
+            {
+                Console.WriteLine($"Editing a contact with a phone number {phoneNumber}.");
+                Console.Write("New FirstName: ");
+                string newFirstName = Console.ReadLine();
+
+                Console.Write("New LastName: ");
+                string newLastName = Console.ReadLine();
+
+                Console.Write("New Phone Number: ");
+                string newPhoneNumber = Console.ReadLine();
+
+                Console.Write("New Email: ");
+                string newEmail = Console.ReadLine();
+
+                Console.Write("New Address: ");
+                string newAddress = Console.ReadLine();
+
+                Contact newContactData = new Contact
+                {
+                    FirstName = newFirstName,
+                    LastName = newLastName,
+                    PhoneNumber = newPhoneNumber,
+                    Email = newEmail,
+                    Address = newAddress
+                };
+
+                contactManager.EditContact(phoneNumber, newContactData);
+            }
+            else
+            {
+                Console.WriteLine("The contact was not found.");
+            }
+        }
+
+        static void SearchContact(ContactManager contactManager)
+        {//SearchContact: Запрашивает у пользователя идентификатор контакта и выводит информацию о контакте, если он найден.
+            Console.Write("Enter your first name, last name, or phone number to search for a contact: ");
+            string identifier = Console.ReadLine();
+
+            Contact foundContact = contactManager.FindContact(identifier);
+
+            if (foundContact != null)
+            {
+                Console.WriteLine($"Contact found: FirstName: {foundContact.FirstName}, LastNaem: {foundContact.LastName}, PhoneNumber: {foundContact.PhoneNumber}, Email: {foundContact.Email}, Address: {foundContact.Address}");
+            }
+            else
+            {
+                Console.WriteLine("Contact was not found.");
+            }
+        }
+
+        static void SaveContactsToFile(ContactManager contactManager)
+        {//SaveContactsToFile и LoadContactsFromFile: Спрашивает пользователя о пути к файлу и сохраняет/загружает контакты в/из файла
+            Console.Write("Enter the path to the file to save the contacts: ");
+            string filePath = Console.ReadLine();
+            contactManager.SaveContactsToFile(filePath);
+        }
+
+        static void LoadContactsFromFile(ContactManager contactManager)
+        {
+            Console.Write("Enter the file path for uploading contacts: ");
+            string filePath = Console.ReadLine();
+            contactManager.LoadContactsFromFile(filePath);
+        }
+
+        static void SendEmail(ContactManager contactManager)
+        {//SendEmail: Запрашивает у пользователя номер телефона контакта, создает электронное письмо и отправляет его.
+            Console.Write("Enter the phone number of the contact you want to send an email to: ");
+            string phoneNumber = Console.ReadLine();
+
+            Contact contact = contactManager.FindContact(phoneNumber);
+
+            if (contact != null)
+            {
+                Console.Write("The subject of the letter: ");
+                string subject = Console.ReadLine();
+
+                Console.Write("The text of the letter: ");
+                string body = Console.ReadLine();
+
+                contactManager.SendEmail(contact, subject, body);
+            }
+            else
+            {
+                Console.WriteLine("The contact was not found.");
+            }
+
+        }
+
+    }
+}
